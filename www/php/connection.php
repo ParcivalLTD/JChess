f5be1593d8a39fb2f8fd6f1fadd5d940d9739c90
Julian Gabriel <juliangabriel570@gmail.com>
2024-09-18 14:09:18 +0200

    Aktualisiere Verbindungseinstellungen in connection.php

--- www/php/connection.php
+++ www/php/connection.php
@@ -1,27 +1,13 @@
 <?php
-// Set the necessary headers for CORS
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
-header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
-
-$host = 'localhost';     // or your host
-$db   = 'default'; // replace with your database name
-$user = 'mysql';      // replace with your database username
-$pass = 'DVFueeDxbjo7tvb0jkXintLK6gIK00sklA5OWejE92IoE8GVBmbvwLIIqXVrJUCl';      // replace with your database password
-$charset = 'utf8mb4';
-
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-$options = [
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES   => false,
-];
+$dsn = "mysql:host=localhost;dbname=default";
+$username = "mysql";
+$password = "DVFueeDxbjo7tvb0jkXintLK6gIK00sklA5OWejE92IoE8GVBmbvwLIIqXVrJUCl";
 
 try {
-    $pdo = new PDO($dsn, $user, $pass, $options);
-    echo "Connected successfully!";
-} catch (\PDOException $e) {
-    throw new \PDOException($e->getMessage(), (int)$e->getCode());
+    $pdo = new PDO($dsn, $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
 }
 ?>
-
